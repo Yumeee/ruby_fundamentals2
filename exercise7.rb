d5b09de4a3cbc2d43185a61d3ce77ded346fbf90
YUMEE <yumeehk@hotmail.com>
2018-05-28 15:15:16 -0400

    finished

--- exercise7.rb
+++ exercise7.rb
@@ -3,18 +3,25 @@ symbol = "==="
 
 def wrap_text(text, signs)
   # puts "#{signs}#{text}#{signs}"
-  puts signs + text + signs
+  return signs + text + signs
 end
 
 # if we use puts --> nil at the line below the wrap_text
 
-wrap_text("hello", symbol)
+puts wrap_text("hello", symbol)
 
 
-def wrap_text_more(text, symbol1, symbol2, symbol3)
-  puts "#{symbol1}#{symbol2}#{symbol3}#{text}#{symbol3}#{symbol2}#{symbol1}"
-end
+# def wrap_text_more(text, symbol1, symbol2, symbol3)
+#   return "#{symbol1}#{symbol2}#{symbol3}#{text}#{symbol3}#{symbol2}#{symbol1}"
+# end
 
-wrap_text_more("hello", "---", symbol, "###")
+# puts wrap_text_more("hello", "---", symbol, "###")
 
-#why doesn't it return for strings?
+text = "hello"
+
+text = wrap_text(text, "###")
+text = wrap_text(text, "===")
+text = wrap_text(text, "---")
+puts text
+
+puts wrap_text(wrap_text(wrap_text("hello","###"), "==="), "---")
